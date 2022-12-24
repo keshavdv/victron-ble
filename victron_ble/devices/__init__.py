a304a7b211b2aa670e5bb4892e85ef9958411920
Carsten Tschach <Carsten@Tschach.com>
2022-12-24 12:39:23 +0000

    Add support for SmartBatterySense

--- victron_ble/devices/__init__.py
+++ victron_ble/devices/__init__.py
@@ -1,18 +1,20 @@
 from construct import Int16ul
 
 from victron_ble.devices.base import Device
-from victron_ble.devices.battery_monitor import BatteryMonitor, BatterySense
+from victron_ble.devices.battery_monitor import BatteryMonitor
+from victron_ble.devices.smart_battery_sense import SmartBatterySense
 
-__all__ = ["Device", "BatteryMonitor"]
+
+__all__ = ["Device", "BatteryMonitor", "SmartBatterySense"]
 
 MODEL_MAPPING = {
-    0xA381: BatteryMonitor,  # BMV-712 Smart
-    0xA382: BatteryMonitor,  # BMV-710H Smart
-    0xA383: BatteryMonitor,  # BMV-712 Smart Rev2
-    0xA389: BatteryMonitor,  # SmartShunt 500A/50mV
-    0xA38A: BatteryMonitor,  # SmartShunt 1000A/50mV
-    0xA38B: BatteryMonitor,  # SmartShunt 2000A/50mV
-    0xA3A4: BatterySense,  # Smart Battery Sense
+    0xA381: BatteryMonitor,     # BMV-712 Smart
+    0xA382: BatteryMonitor,     # BMV-710H Smart
+    0xA383: BatteryMonitor,     # BMV-712 Smart Rev2
+    0xA389: BatteryMonitor,     # SmartShunt 500A/50mV
+    0xA38A: BatteryMonitor,     # SmartShunt 1000A/50mV
+    0xA38B: BatteryMonitor,     # SmartShunt 2000A/50mV
+    0xA3A4: SmartBatterySense,  # Smart Battery Sense
 }
 
 
