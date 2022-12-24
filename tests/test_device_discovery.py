8ec9e5af2c19a165a55d9fad68efa7840bfed36a
Keshav Varma <keshavdv@gmail.com>
2022-12-23 20:39:46 -0500

    Fix formatting

--- tests/test_device_discovery.py
+++ tests/test_device_discovery.py
@@ -4,5 +4,6 @@ from victron_ble.devices import detect_device_type, BatteryMonitor, Device
 def test_unknown_device_type():
     assert detect_device_type(bytes.fromhex("10020000")) == None
 
+
 def test_battery_monitor_discovery():
     assert detect_device_type(bytes.fromhex("100289a3")) == BatteryMonitor
