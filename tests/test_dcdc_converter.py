a8d22016a884ce18cd505b5afd4ac628fa26fb7d
Stefano Rivera <stefano@rivera.za.net>
2023-03-30 17:46:02 -0400

    Use decrypted data in tests
    Except for one end-to-end test for each object type.
    
    This makes for easier to understand and simpler tests.
    Allows fabrication of test data for corner cases like NA values.

--- tests/test_dcdc_converter.py
+++ tests/test_dcdc_converter.py
@@ -1,13 +1,14 @@
-from victron_ble.devices.dcdc_converter import DcDcConverter
+from victron_ble.devices.dcdc_converter import DcDcConverter, DcDcConverterData
 from victron_ble.devices.base import OperationMode, OffReason, ChargerError
 
 
 class TestDcDcConverter:
-    def test_parse_data(self) -> None:
+    def test_end_to_end_parse(self) -> None:
         data = "1000c0a304121d64ca8d442b90bbdf6a8cba"
         actual = DcDcConverter("64ba49f1a8562e45197a8e1fe50d7658").parse(
             bytes.fromhex(data)
         )
+        assert isinstance(actual, DcDcConverterData)
 
         assert actual.get_charge_state() == OperationMode.OFF
         assert actual.get_charger_error() == ChargerError.NO_ERROR
@@ -17,3 +18,17 @@ class TestDcDcConverter:
         assert (
             actual.get_model_name() == "Orion Smart 12V|12V-18A Isolated DC-DC Charger"
         )
+
+    def parse_decrypted(self, decrypted: str) -> DcDcConverterData:
+        parsed = DcDcConverter(None).parse_decrypted(
+            bytes.fromhex(decrypted)
+        )
+        return DcDcConverterData(None, parsed)
+
+    def test_parse(self) -> None:
+        actual = self.parse_decrypted("00002305ff7f80000000cbdd494cc5d1")
+        assert actual.get_charge_state() == OperationMode.OFF
+        assert actual.get_charger_error() == ChargerError.NO_ERROR
+        assert actual.get_input_voltage() == 13.15
+        assert actual.get_output_voltage() == 0
+        assert actual.get_off_reason() == OffReason.ENGINE_SHUTDOWN
