9f0b680eaf96169e705f53aa500b5843977d8ce0
Jeff Brown <jeff.brown@gmail.com>
2024-02-23 14:39:31 -0800

    Make code style more consistent with rest of project

--- victron_ble/devices/inverter.py
+++ victron_ble/devices/inverter.py
@@ -10,9 +10,9 @@ from victron_ble.devices.base import (
 
 
 class InverterData(DeviceData):
-    def get_charge_state(self) -> OperationMode:
+    def get_device_state(self) -> OperationMode:
         """
-        Return an enum indicating the current charging state
+        Return an enum indicating the current device state
         """
         return self._data["device_state"]
 
@@ -28,23 +28,23 @@ class InverterData(DeviceData):
         """
         return self._data["battery_voltage"]
 
-    def get_AC_apparent_power(self) -> int:
+    def get_ac_apparent_power(self) -> int:
         """
         Return the output AC power in voltampere
         """
-        return self._data["AC_apparent_power"]
+        return self._data["ac_apparent_power"]
 
-    def get_AC_voltage(self) -> float:
+    def get_ac_voltage(self) -> float:
         """
         Return the output AC voltage in volts
         """
-        return self._data["AC_voltage"]
+        return self._data["ac_voltage"]
 
-    def get_AC_current(self) -> float:
+    def get_ac_current(self) -> float:
         """
         Return the output AC current in amperes
         """
-        return self._data["AC_current"]
+        return self._data["ac_current"]
 
 class Inverter(Device):
     data_type = InverterData
@@ -57,16 +57,16 @@ class Inverter(Device):
         # Input voltage reading in 0.01V increments
         "battery_voltage" / Int16sl,
         # Output AC power in 1VA
-        "AC_apparent_power" / Int16ul,
+        "ac_apparent_power" / Int16ul,
 
         # Output AC voltage in 0.01V
         # The next 15 bits indicate the voltage in 0.01 V (0 .. 327.66 V | 0x7FFF)
-        "AC_voltage"/ Int16ul,
+        "ac_voltage"/ Int16ul,
 
         # Output AC current in 0.1A
         # The next 11 bits identify the current in 0.1 A (0 .. 204.6 A | 0x7FF)
         # enconding in little endian (Int16ul)
-        "AC_current"/ Int16ul,
+        "ac_current"/ Int16ul,
         GreedyBytes,
     )
 
@@ -77,7 +77,7 @@ class Inverter(Device):
             "device_state": OperationMode(pkt.device_state),
             "alarm": pkt.alarm,
             "battery_voltage": (pkt.battery_voltage) / 100,
-            "AC_apparent_power": pkt.AC_apparent_power,
-            "AC_voltage": (pkt.AC_voltage & 0x7FFF) / 100,
-            "AC_current": ((pkt.AC_voltage >> 15 | pkt.AC_current << 1) & 0x7FF) / 10,
+            "ac_apparent_power": pkt.ac_apparent_power,
+            "ac_voltage": (pkt.ac_voltage & 0x7FFF) / 100,
+            "ac_current": ((pkt.ac_voltage >> 15 | pkt.AC_current << 1) & 0x7FF) / 10,
         }
